e91c8730d53007e55dab1cbf8e94f3c18352ef04
Silva Robbins <silva.robbins@gmail.com>
2021-05-31 17:52:27 +0300

    tehtävä 1.8

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,27 +1,58 @@
 import React, { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <>
+      average {(good-bad)/(good+neutral+bad)} <br></br>
+      positive {good/(good+neutral+bad)}
+    </>
+  )
+}
+
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+  }
+
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>
+      <button onClick={handleGoodClick}>
           Good
       </button>
-      <button onClick={() => setNeutral(neutral + 1)}>
+      <button onClick={handleNeutralClick}>
           Neutral
       </button>
-      <button onClick={() => setBad(bad + 1)}>
+      <button onClick={handleBadClick}>
           Bad
       </button>
       <h1>statistics</h1>
-      <body>good {good}</body>
-      <body>neutral {neutral}</body>
-      <body>bad {bad}</body>
+      <p>
+        good {good} <br></br>
+        neutral {neutral} <br></br>
+        bad {bad} <br></br>
+        all {all} <br></br>
+        <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
+      </p>
     </div>
   )
 }
