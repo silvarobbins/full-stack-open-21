0bea8b447546b2fa59aace36b17d602f622d4f01
Silva Robbins <silva.robbins@gmail.com>
2021-09-08 01:53:19 +0300

    osa 2 valmis

--- osa2/puhelinluettelo/src/components/Persons.js
+++ osa2/puhelinluettelo/src/components/Persons.js
@@ -1,11 +1,11 @@
-import personsService from "../services/persons"
-
-const Persons = ({personsToShow}) => {
+const Persons = ({ personsToShow, deletePerson}) => {
     return (
     personsToShow.map(person => 
         <form 
-            key={person.id} 
-            onSubmit={(event) => {personsService.del(person.id)}}>
+            key={person.name} 
+            onSubmit={(event) => {
+                event.preventDefault()
+                deletePerson(person.id)}}>
                 {person.name} {person.number}<button type="submit">delete</button>
         </form>
         )
