f3ba6acd3137f5f94da8aa56f68cf27517785ea5
Silva Robbins <silva.robbins@gmail.com>
2021-12-15 01:29:31 +0200

    4.2

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,46 +1,10 @@
+const app = require('./app') // varsinainen Express-sovellus
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const password = process.argv[2]
+const server = http.createServer(app)
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.rg7ao.mongodb.net/blogilista?retryWrites=true&w=majority`
-mongoose.connect(url)
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
