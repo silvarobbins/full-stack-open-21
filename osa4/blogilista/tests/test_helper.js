ac27bf3e1142baee9afa99a19c17d15f9724ddb4
Silva Robbins <silva.robbins@gmail.com>
2022-01-04 20:01:32 +0200

    4.18 complete, blog post with token not functioning

--- osa4/blogilista/tests/test_helper.js
+++ osa4/blogilista/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -15,11 +16,16 @@ const initialBlogs = [
   }
 ]
 
-const BlogsInDb = async () => {
+const blogsInDb = async () => {
   const blogs = await Blog.find({})
   return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 module.exports = {
-  initialBlogs, BlogsInDb
+  initialBlogs, blogsInDb, usersInDb
 }
--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -13,16 +13,19 @@ const unknownEndpoint = (request, response) => {
   }
   
   const errorHandler = (error, request, response, next) => {
-    logger.error(error.message)
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
       return response.status(400).json({ error: error.message })
-    }
+    } else if (error.name === 'JsonWebTokenError') {
+      return response.status(401).json({ error: 'invalid token' })
   
-    next(error)
   }
+
+  logger.error(error.message)
+  next(error)
+}
   
   module.exports = {
     requestLogger,
