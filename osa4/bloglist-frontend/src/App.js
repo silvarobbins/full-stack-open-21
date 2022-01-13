9265a3f844a3c06420c190601fc7edc9a43f85e2
Silva Robbins <silva.robbins@gmail.com>
2022-01-13 21:18:03 +0200

    5.9 sort by likes

--- osa4/bloglist-frontend/src/App.js
+++ osa4/bloglist-frontend/src/App.js
@@ -127,13 +127,18 @@ const App = () => {
     </div>
   )
   
-  const blogList = (user) => (
-    <div>
+  const blogList = () => {
+    blogs.sort((a,b) => b.likes-a.likes)
+    console.log(blogs)
+    return(
+      <div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} user={blog.user} likeBlog={likeBlog}/>
       )}
-    </div>
-  )
+      </div>
+    )
+  }
+    
 
   if(user === null) {
     return (
