dc32fc9dc55832dae25fa4149d45a24c00141cd8
Silva Robbins <silva.robbins@gmail.com>
2022-03-02 16:08:46 +0200

    8.4.

--- osa8/kirjasto/kirjasto-backend/index.js
+++ osa8/kirjasto/kirjasto-backend/index.js
@@ -91,6 +91,7 @@ let books = [
 const typeDefs = gql`
   type Book {
     title: String!
+    published: Int!
     author: String!
     id: ID!
     genres: [String]
@@ -99,19 +100,35 @@ const typeDefs = gql`
   type Author {
     name: String!
     id: ID!
-    born: Int!
+    born: Int
+    bookCount: Int!
   }
 
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks(author: String): [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: (root, args) => {
+      if (args.author) {
+        return books.filter((b) => b.author === args.author)
+      } else {
+        return books
+      }
+    },
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => {
+      return books.filter((b)=> b.author===root.name).length
+    }
   }
 }
 
